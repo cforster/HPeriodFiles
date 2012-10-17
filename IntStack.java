e1c1f470c2c0e3ce2f59447ece42e7214d7b3e66
Aleksandar Kiprovski <student@C02G20VWDDR0.dalton.org>
2012-10-17 10:51:40 -0400

    Aleks and Charlie wrote these in class with no help from anybody.

---
+++ IntStack.java
@@ -0,0 +1,51 @@
+class IntStack
+{
+    //todo:
+    //write resize
+
+    //declarations:
+    int[] stack;
+    int top=0;
+
+    IntStack(int size)
+    {
+	stack = new int[size];
+    }
+
+    int pop()
+    {
+	if(isEmpty()) return -1;
+	return stack[--top];
+    }
+
+
+    void push(int i)
+    {
+	stack[top++]=i;
+	if(top==stack.length) resize();
+    }
+
+
+    int peek()
+    {
+	if(isEmpty()) return -1;
+	return stack[top-1];
+    }
+
+    boolean isEmpty()
+    {
+	return top==0;
+    }
+
+    private void resize()
+    {
+	int[] newStack = new int[stack.length*2];
+	
+	for(int i= 0; i< top; i++) {
+	    stack[i]=newStack[i];
+	}
+	 
+	//System.out.println("resizing...");
+	stack=newStack;
+    }
+}
---
+++ IntStackTest.java
@@ -0,0 +1,25 @@
+class IntStackTest
+{
+    public static void main(String[] args)
+    {
+	IntStack is = new IntStack(2);
+
+	System.out.println(is.isEmpty());
+
+	is.push(3);
+	is.push(4);
+	is.push(10);
+
+	System.out.println(is.peek());
+	System.out.println(is.pop());
+	System.out.println(is.pop());
+	System.out.println(is.pop());
+	System.out.println(is.pop());
+	System.out.println(is.pop());
+	System.out.println(is.pop());
+	System.out.println(is.pop());
+	System.out.println(is.isEmpty());
+	System.out.println(is.pop());
+    }
+}
+	
