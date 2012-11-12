da04dc7b25f6140a61b16510f2cd0e843501b41c
Charles Forster <charlie@forsters.com>
2012-11-12 14:28:32 -0500

    Revert "Map class completed and tested"
    This reverts commit c6888065ec0cd0b9120f30e91eab1e3660c038e7.

--- FreqDict.java
+++ FreqDict.java
@@ -7,47 +7,25 @@ class FreqDict {
 	
 	//split up my words
 	String[] words = document.split(" ");
-
-	//working on bonus commas(unfinished)
-	/**for(int i = 0; i<args.length; i++)
-	    {
-		if(args[i]== ","|| "."||"!"||"?"||";")
-		  {
-		      args[i]== " ";
-		  }
-
-		
-		  }//closes for**/
 		
 	//loop through and put them in
 	for(String word : words) {
 	    if(word.length()==0) continue;
 
 	    //is the word in the map?
+	    //if so:
+	         //increment the value at that key by one
+	    //otherwise:
+	         //insert the key into the map with value one.
 
-	    if(wordcount.containsKey(word))
-		{
-		    int count = wordcount.get(word);
-		    wordcount.put(word, count+1);
-		    
-		}
-	    else
-	    {
-		wordcount.put(word, 1);
-	    }
-
-	}//closes for loop
+	}
 	
+
 	//print out the whole thing
-	//System.out.println(wordcount);
-
-
 	//for all the keys in the map
-	for(String key : wordcount.keySet())
-	    {
 	     //print the key and the value
-		System.out.println(key +":"+ wordcount.get(key));
-	    }
+	
+
 
 	//sample code:
 	//wordcount.put("O", 5);
