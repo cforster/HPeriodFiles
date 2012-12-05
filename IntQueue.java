bc7679f9f25e6881a968b2864068da8640dd3990
Grace Monk <grace_monk@dalton.org>
2012-12-05 10:34:51 -0500

    changes recent for mean

--- IntQueue.java
+++ IntQueue.java
@@ -1,4 +1,6 @@
 class IntQueue {
+    //Grace Monk and Elana Simon
+
 
     private int[] q;
     private int front=0;
@@ -113,7 +115,6 @@ class IntQueue {
      *@param s the place you want to start
      *@param e the place you want to end
      */
-
     public IntQueue subQ(int s, int e) {
 	IntQueue subQ = new IntQueue();
 	if(s<e){
@@ -135,20 +136,21 @@ class IntQueue {
     //Sonia and Jenna
     //Mean - gets the average of the Queue 
 
-	int size = front-1;
+
 	int mean()
 	{
-		int total = 0;
-		for (int i=0; i < top+1; i++);
-		{
-		    total == total+stack[i];
-		}
-		total = total/top+1;
-		return total;
+	    int size = front-1;
+	    int total = 0;
+	    for (int i=0; i < top+1; i++);
+	    {
+		total+=stack[i];
+	    }
+	    total = total/top+1;
+	    return total;
 	}
 		    
 		
-   }
+   
 
 	/*Cut
 	 *Allows the user to add an int to the front of the queue
